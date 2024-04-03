c52afaf7ca3dea48468c8646de4cc91f785867c1
larissaElMajdobLima <larissa.lima@bandtec.com.br>
2024-04-02 22:58:32 -0300

    FEAT: Search field

--- src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/components/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -12,22 +12,32 @@ export class ListarPensamentoComponent implements OnInit {
   listaPensamento: Pensamento[] = [];
   paginaAtual: number = 1;
   temMaisPensamentos: boolean = true;
+  filtro: string = "";
 
   constructor(private service: PensamentoService) { }
 
   ngOnInit(): void {
-    this.service.listar(this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamento = listaPensamentos;
     });
   }
 
   carregarMaisPensamentos() {
-    this.service.listar(++this.paginaAtual).subscribe((listaPensamentos) => {
+    this.service.listar(++this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
       this.listaPensamento.push(...listaPensamentos);
       if(!listaPensamentos.length) {
         this.temMaisPensamentos = false;
       }
     });
+  }
+
+  pesquisarPensamentos() {
+    this.paginaAtual = 1;
+    this.temMaisPensamentos = true;
+
+    this.service.listar(this.paginaAtual, this.filtro).subscribe((listaPensamentos) => {
+      this.listaPensamento = listaPensamentos;
+    })
   }
 
 }
